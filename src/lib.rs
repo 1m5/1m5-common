5a9f69433f8e6a463f6ebb90a722c84abeb58356
objectorange <objectorange@1m5.io>
2020-03-22 13:40:34 -0700

    enum to Upper camel case - idiomatic Rust

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,19 @@
 #[derive(Debug)]
 pub enum ManCon {
     // 1DN Only w/ Random Configurable Delays: 10-100 Relays (~2sec-90days) / 20-200 Round-trip (~4sec-90days)
-    NEO,
+    Neo,
     // 1DN + I2P w/ Random Configurable Delays: 5 Relays (~1sec-6minutes) / 10 Round-trip (~2sec-1day)
-    EXTREME,
+    Extreme,
     // I2P w/ Random Configurable Delays: 4 Relays (~800ms-6minutes) / 8 Round-trip (~1.8sec-12minutes)
-    VERYHIGH,
+    VeryHigh,
     // I2P: 4 Relays (~800ms) / 8 Round-trip (~1.8sec)
-    HIGH,
+    High,
     // TOR: 3 Relays (~600ms) / 6 Round-trip (~1.4sec)
-    MEDIUM,
+    Medium,
     // VPN: 1 Relay (~200ms) / 2 Round-trip (~600ms)
-    LOW,
+    Low,
     // HTTPS: 0 Relays
-    NONE,
+    None,
     // Unspecified
-    UNKNOWN
+    Unknown
 }
