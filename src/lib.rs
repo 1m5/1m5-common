b46c833abd741749d681e3c4e96169391a73d084
objectorange <objectorange@1m5.io>
2020-03-23 00:14:30 -0700

    add numbers to ManCon enum

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,20 @@
 /// Maneuvering Condition
 #[derive(Debug)]
 pub enum ManCon {
-    // 1DN Only w/ Random Configurable Delays: 10-100 Relays (~2sec-90days) / 20-200 Round-trip (~4sec-90days)
-    Neo,
-    // 1DN + I2P w/ Random Configurable Delays: 5 Relays (~1sec-6minutes) / 10 Round-trip (~2sec-1day)
-    Extreme,
-    // I2P w/ Random Configurable Delays: 4 Relays (~800ms-6minutes) / 8 Round-trip (~1.8sec-12minutes)
-    VeryHigh,
-    // I2P: 4 Relays (~800ms) / 8 Round-trip (~1.8sec)
-    High,
-    // TOR: 3 Relays (~600ms) / 6 Round-trip (~1.4sec)
-    Medium,
-    // VPN: 1 Relay (~200ms) / 2 Round-trip (~600ms)
-    Low,
-    // HTTPS: 0 Relays
-    None,
-    // Unspecified
-    Unknown
+    /// Unspecified
+    Unknown     = 0,
+    /// HTTPS: 0 Relays
+    None        = 1,
+    /// VPN: 1 Relay (~200ms) / 2 Round-trip (~600ms)
+    Low         = 2,
+    /// TOR: 3 Relays (~600ms) / 6 Round-trip (~1.4sec)
+    Medium      = 3,
+    /// I2P: 4 Relays (~800ms) / 8 Round-trip (~1.8sec)
+    High        = 4,
+    /// I2P w/ Random Configurable Delays: 4 Relays (~800ms-6minutes) / 8 Round-trip (~1.8sec-12minutes)
+    VeryHigh    = 5,
+    /// 1DN + I2P w/ Random Configurable Delays: 5 Relays (~1sec-6minutes) / 10 Round-trip (~2sec-1day)
+    Extreme     = 6,
+    /// 1DN Only w/ Random Configurable Delays: 5-10 Relays (~2sec-90days) / 10-20 Round-trip (~4sec-90days)
+    Neo         = 7
 }
