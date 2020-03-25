75ddd96192e57ed889107466b058853a8c8d17f8
objectorange <objectorange@1m5.io>
2020-03-24 20:37:37 -0700

    impl From trait

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,27 @@
 /// Maneuvering Condition
 #[derive(Debug)]
 pub enum ManCon {
-    /// Unspecified
     Unknown     = 0,
-    /// HTTPS: 0 Relays
     None        = 1,
-    /// VPN: 1 Relay (~200ms) / 2 Round-trip (~600ms)
     Low         = 2,
-    /// TOR: 3 Relays (~600ms) / 6 Round-trip (~1.4sec)
     Medium      = 3,
-    /// I2P: 4 Relays (~800ms) / 8 Round-trip (~1.8sec)
     High        = 4,
-    /// I2P w/ Random Configurable Delays: 4 Relays (~800ms-6minutes) / 8 Round-trip (~1.8sec-12minutes)
     VeryHigh    = 5,
-    /// 1DN + I2P w/ Random Configurable Delays: 5 Relays (~1sec-6minutes) / 10 Round-trip (~2sec-1day)
     Extreme     = 6,
-    /// 1DN Only w/ Random Configurable Delays: 5-10 Relays (~2sec-90days) / 10-20 Round-trip (~4sec-90days)
     Neo         = 7
+}
+
+impl From<u8> for ManCon {
+    fn from(num: u8) -> Self {
+        match num {
+            1 => ManCon::None,
+            2 => ManCon::Low,
+            3 => ManCon::Medium,
+            4 => ManCon::High,
+            5 => ManCon::VeryHigh,
+            6 => ManCon::Extreme,
+            7 => ManCon::Neo,
+            _ => ManCon::Unknown
+        }
+    }
 }
